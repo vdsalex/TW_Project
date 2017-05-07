e42cc48da40332e2548a2f2806f436ffe2d04d72
vdsalex <voaides.alex@gmail.com>
2017-05-07 19:39:26 +0300

    Pagina de login e responsive. Mission accomplished.

--- login_page/login_page.js
+++ login_page/login_page.js
@@ -1,9 +1,9 @@
 function openTab(buttonId, button, tabcontentId)
 {
+    if(currentButton === buttonId)return;
+
 	var i, tabcontent, tablinks;
 	var theOtherButton;
-	var loginBox;
-	var description;
 
 	tabcontent = document.getElementsByClassName("tabcontent");
 
@@ -25,31 +25,52 @@ function openTab(buttonId, button, tabcontentId)
 	}
 
 	button.style.boxShadow = "1px 1px 2px 1px MidnightBlue";
-	loginBox = document.getElementById("loginBox");
-	//description = document.getElementById("description");
 
-
-	if(buttonId.localeCompare("loginButton") == 0)
+	if(buttonId.localeCompare("loginButton") === 0)
 	{
 		theOtherButton = document.getElementById("signUpButton");
 		theOtherButton.style.boxShadow = "inset 3px -3px 5px 0 MidnightBlue";
 
-		loginBox.style.height = "300px";
-		//description.style.top = "22.1%";
+        if(!first)
+        {
+            if(window.screen.width <= 444)loginBox.style.height = String(loginBox.offsetHeight - 130) + "px";
+            else loginBox.style.height = String(loginBox.offsetHeight - 30) + "px";
+        }
+        else first = false;
 	}
 	else
 	{
 		theOtherButton = document.getElementById("loginButton");
 		theOtherButton.style.boxShadow = "inset -3px -3px 5px 0 MidnightBlue";
 
-		loginBox.style.height = "330px";
-		//description.style.top = "19.6%";
+		if(window.screen.width <= 444)loginBox.style.height = String(loginBox.offsetHeight + 130) + "px";
+		else loginBox.style.height = String(loginBox.offsetHeight + 30) + "px";
 	}
 
 	button.style.color = "rgb(26, 18, 142)";
 	button.style.backgroundColor = "Transparent";
 
 	document.getElementById(tabcontentId).style.display = "block";
+
+	currentButton = buttonId;
 }
 
+//Trebuie setata dimensiunea initiala pentru loginBox atunci cand se iese din modul Inspect
+function onResize()
+{
+    if(window.screen.width >= 660)
+    {
+        if(currentButton.localeCompare("loginButton") === 0)loginBox.style.height = "300px";
+        else loginBox.style.height = "330px";
+    }
+}
+
+var first = true;
+var currentButton = null;
+var loginBox = document.getElementById("loginBox");
+
 document.getElementById("loginButton").click();
+
+currentButton = "loginButton";
+
+onResize();
