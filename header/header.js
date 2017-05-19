fb2494cf8389c19d0618c8f851d38789db8f3b87
vdsalex <voaides.alex@gmail.com>
2017-05-19 18:13:01 +0300

    Header-ul e acum responsive.

--- header/header.js
+++ header/header.js
@@ -1,6 +1,8 @@
 // Get the modal
 var modal = document.getElementById('myModal');
 
+var body = document.getElementsByClassName('body');
+
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById('pf-photo');
 var modalImg = document.getElementById("pf-pht");
@@ -9,6 +11,7 @@ img.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
+    body.style.overflow = "hidden";
 }
 
 // Get the <span> element that closes the modal
@@ -17,4 +20,5 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() { 
     modal.style.display = "none";
+    body.style.overflow = "auto";
 }
