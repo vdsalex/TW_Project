5aa6e7fb0758d748101e9083c63b53eb02146e00
andreirusu96 <andrew_x_2005@yahoo.com>
2017-06-04 19:23:27 +0300

    Redesigned project architecture

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,116 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+
+class UserController extends Controller
+{
+
+    public function getHome()
+    {
+        return view('pages/home',['user'=> Auth::user()]);
+    }
+
+    public function getLogin()
+    {
+        return view('pages/login',['user'=> Auth::user()]);
+    }
+
+    public function getMyMemories()
+    {
+        return view('pages/my_memories',['user'=> Auth::user()]);
+    }
+
+    public function getUpload()
+    {
+        return view('pages/upload',['user'=> Auth::user()]);
+    }
+
+    public function getProfile()
+    {
+        return view('pages/profile',['user'=> Auth::user()]);
+    }
+    public function getAdvancedSearch()
+    {
+        return view('pages/advanced_search',['user'=> Auth::user()]);
+    }
+
+    public function postSignUp(Request $request)
+    {
+        $this->validate($request,[
+            'email' => 'required|email|unique:users',
+            'first_name' => 'required|max:100',
+            'last_name' =>'required|max:100',
+            'password' => 'required|min:4',
+            'username' => 'required|unique:users'
+        ]);
+        $username= $request['username'];
+        $password= bcrypt($request['password']);
+        $email= $request['e_mail'];
+        $first_name= $request['first_name'];
+        $last_name= $request['last_name'];
+        $address=$request['address'];
+
+        $newUser=User::create(['username'=>$username,'password'=>$password,'email'=>$email,'first_name'=>$first_name,'last_name'=>$last_name,'address'=>$address]);
+        dd($newUser);
+
+        Auth::login($newUser);
+
+        return redirect()->route('home');
+    }
+
+    public function postSignIn(Request $request)
+    {
+        $this->validate($request,[
+            'password' => 'required',
+            'username' => 'required'
+        ]);
+
+        $remember=$request->has('rememberMeCBox');
+
+        if (Auth::attempt(['username'=>$request['username'],'password'=>$request['password']],$remember))
+        {
+            return redirect()->route('home');
+        }
+        else
+            return redirect()->back();
+    }
+
+    public function getLogout()
+    {
+        Auth::logout();
+        return redirect()->route('home');
+    }
+
+    public function postSaveAccount(Request $request)
+    {
+       /* $this->validate($request,[
+
+        ]);
+       */
+       $user=Auth::user();
+       $user->first_name=$request['first_name'];
+       $user->update();
+       $file=$request->file('image');
+
+       /*to implement file extention validation */
+
+       $filename=$user->username . '-'.$user->id.'\\'.'profile.jpg';
+       if ($file)
+       {
+           Storage::disk('local')->put($filename, File::get($file));
+       }
+       return redirect()->route('profile');
+    }
+
+    public function getUserImage($filename)
+    {
+        $file=Storage::disk('local')->get($filename);
+        return Response($file, 200);
+    }
+
+}
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,84 @@
+{!! Html::style('css/header.css') !!}
+{!! Html::style('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css') !!}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+<body>
+    
+	<nav class="navbar navbar-default navbar-fixed-top" id="main-nav" data-spy="affix" data-offset-top="14">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href={{route('home')}} id="DBrand">
+					<span>DiLy.</span>
+					<span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+				</a>
+			</div>
+			<div class="navbar-form navbar-left" id="leftNavbar">
+				<div class="dropdown" id="dropdown1">
+					<span id="menuSpan" data-toggle="dropdown" aria-expanded="true" role="menu">MENU</span>
+					<ul class="dropdown-menu" aria-labelledby="menuSpan">
+						<li><a href="{{route('upload')}}">UPLOAD</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="{{route('memories')}}">MY MEMORIES</a></li>
+						<li role="separator" class="divider"></li>
+						<li><a href="{{route('search')}}">ADVANCED SEARCH</a></li>
+						<li role="separator" class="divider"></li>
+						<li>
+							<div class="input-group">
+								<input spellcheck="false" type="text" id="searchDropdwn" class="form-control" placeholder="Search for..">
+								<button class="btn btn-default" type="submit">
+									<span class="glyphicon glyphicon-search form-control-feedback" aria-hidden="true"></span>
+								</button>
+							</div>
+						</li>
+					</ul>
+				</div>
+				<ul class="nav navbar-nav">
+					<li><a href={{route('upload')}} id="uploadText">UPLOAD</a></li>
+					<li><a href={{route('memories')}} id="MyMemText">MY MEMORIES</a></li>
+					<li>
+						<div class="input-group">
+							<input spellcheck="false" type="text" id="search" class="form-control" placeholder="Search for..">
+							<button class="btn btn-default" type="submit">
+								<span class="glyphicon glyphicon-search form-control-feedback" aria-hidden="true"></span>
+							</button>
+						</div>
+					</li>
+					<li><a href={{route('search')}} id="advSearchText">ADVANCED SEARCH</a></li>
+				</ul>
+			</div>
+			<form class="navbar-form navbar-right">
+				@if (Storage::disk('local')->has($user->username . '-'.$user->id.'\\'.'profile.jpg'))
+					<img src="{{ route('account.image', ['filename' => $user->username . '-'.$user->id.'\\'.'profile.jpg']) }}" alt="Profile image" id="pf-photo">
+				@else
+					<img src="{{ route('account.image', ['filename' => 'default_profile_img.jpg']) }}" alt="Profile image" id="pf-photo">
+				@endif
+
+				<span>
+					Welcome, <a href={{route('profile')}}>{{$user->first_name . ' ' . $user->last_name}}</a>
+					<br>
+					<a href={{route('logout')}}>Logout</a>
+				</span>
+			</form>
+		</div>
+	</nav>
+
+    <div id="myModal" class="modal">
+        <span class="close">×</span>
+		@if (Storage::disk('local')->has($user->username . '-'.$user->id.'\\'.'profile.jpg'))
+        <img src="{{ route('account.image', ['filename' => $user->username . '-'.$user->id.'\\'.'profile.jpg']) }}" class="modal-content" id="pf-pht" alt="Profile Photo">
+		@else
+			<img src="{{ route('account.image', ['filename' => 'default_profile_img.jpg']) }}" class="modal-content" id="pf-pht" alt="Profile Photo">
+		@endif
+        <div id="caption"></div>
+    </div>
+</body>
+</html>
+
+{!! Html::script('https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js') !!}
+{!! Html::script('https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js') !!}
+{!! Html::script('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js') !!}
+{!! Html::script('js/header.js') !!}
