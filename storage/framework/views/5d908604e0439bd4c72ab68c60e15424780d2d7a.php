29ba84ccda3da5c96a72f90073cb520a13def27f
andreirusu96 <andrew_x_2005@yahoo.com>
2017-06-04 20:08:31 +0300

    Deleted files

--- storage/framework/views/5d908604e0439bd4c72ab68c60e15424780d2d7a.php
+++
@@ -1,6 +0,0 @@
-<?php
-if(DB::connection()->getDatabaseName())
-{
-   echo "Yes! successfully connected to the DB: " . DB::connection()->getDatabaseName();
-}
-else echo "No! successfully connected to the DB: " . DB::connection()->getDatabaseName();
