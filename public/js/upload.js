eb28a9e324cb7c060bdc36f48527489f7c785b15
Leustean Andrei <leuandrei01@gmail.com>
2017-06-11 15:09:57 +0300

    individualizat id-uri upload

--- public/js/upload.js
+++ public/js/upload.js
@@ -6,7 +6,7 @@ function displayPhoto()
     if (memOptions.style.display === 'none')
         memOptions.style.display = 'inline';
    // memOptions.innerHTML = "<br><label for=\"description\">Descriere: </label><input type=\"text\" name=\"picDesc\"> <br>Locatie:  <input type=\"text\" name=\"picLoc\"> <br><input type=\"Submit\" name=\"upPic\" value=\"Upload\">";
-    memOptions.innerHTML = "<form class=\"form-horizontal\"> <fieldset><div class=\"control-group\"> <label class=\"control-label\" for=\"title\">Title</label> <div class=\"controls\"> <input id=\"title\" name=\"title\" type=\"text\" placeholder=\"Name\" class=\"input-xlarge\"> </div> </div> <div class=\"control-group\"> <label class=\"control-label\" for=\"Description\">Description</label> <div class=\"controls\"> <textarea id=\"Description\" name=\"Description\"></textarea> </div> </div> <!-- File Button --> <div class=\"control-group\"> <label class=\"control-label\" for=\"filebutton\">Picture File</label> <div class=\"controls\"> <input id=\"filebutton\" name=\"filebutton\" class=\"input-file\" type=\"file\"> </div> </div> <!-- Button --> <div class=\"control-group\"> <label class=\"control-label\" for=\"singlebutton\"></label> <div class=\"controls\"> <button id=\"singlebutton\" name=\"singlebutton\" class=\"btn btn-info\">Upload</button> </div> </div> </fieldset> </form>"
+    memOptions.innerHTML = "<form class=\"form-horizontal picture-upload\"> <fieldset><div class=\"control-group\"> <label class=\"control-label\" for=\"pictureTitle\">Title</label> <div class=\"controls\"> <input id=\"pictureTitle\" name=\"pictureTitle\" type=\"text\" placeholder=\"Name\" class=\"input-xlarge\"> </div> </div> <div class=\"control-group\"> <label class=\"control-label\" for=\"pictureDescription\">Description</label> <div class=\"controls\"> <textarea id=\"pictureDescription\" name=\"pictureDescription\" class=\"form-control\" rows=\"5\"></textarea> </div> </div> <!-- File Button --> <div class=\"control-group\"> <label class=\"control-label\" for=\"pictureFileButton\">Picture File</label> <div class=\"controls\"> <input id=\"pictureFileButton\" name=\"pictureFileButton\" class=\"input-file\" type=\"file\"> </div> </div> <!-- Button --> <div class=\"control-group\"> <label class=\"control-label\" for=\"uploadPictureButton\"></label> <div class=\"controls\"> <button id=\"uploadPictureButton\" name=\"uploadPictureButton\" class=\"btn btn-info\">Upload</button> </div> </div> </fieldset> </form>"
 }
 
 function displayFilm()
@@ -21,32 +21,32 @@ function displayFilm()
 <div class=\"control-group\">\
     <label class=\"control-label\" for=\"title\">Title</label>\
     <div class=\"controls\">\
-    <input id=\"title\" name=\"title\" type=\"text\" placeholder=\"Name\" class=\"input-xlarge\">\
+    <input id=\"videoTitle\" name=\"videoTitle\" type=\"text\" placeholder=\"Name\" class=\"input-xlarge\">\
 \
     </div>\
     </div>\
 \
     <!-- Textarea -->\
     <div class=\"control-group\">\
-    <label class=\"control-label\" for=\"Description\">Description</label>\
+    <label class=\"control-label\" for=\"videoDescription\">Description</label>\
     <div class=\"controls\">\
-    <textarea id=\"Description\" name=\"Description\"></textarea>\
+    <textarea id=\"Description\" name=\"videoDescription\"></textarea>\
     </div>\
     </div>\
     \
     <!-- File Button -->\
 <div class=\"control-group\">\
-    <label class=\"control-label\" for=\"filebutton\">Video File</label>\
+    <label class=\"control-label\" for=\"videoFileButton\">Video File</label>\
 <div class=\"controls\">\
-    <input id=\"filebutton\" name=\"filebutton\" class=\"input-file\" type=\"file\">\
+    <input id=\"videoFileButton\" name=\"videoFileButton\" class=\"input-file\" type=\"file\">\
     </div>\
     </div>\
     \
     <!-- Button -->\
     <div class=\"control-group\">\
-    <label class=\"control-label\" for=\"singlebutton\"></label>\
+    <label class=\"control-label\" for=\"videoUploadButton\"></label>\
     <div class=\"controls\">\
-    <button id=\"singlebutton\" name=\"singlebutton\" class=\"btn btn-info\">Upload</button>\
+    <button id=\"videoUploadButton\" name=\"videoUploadButton\" class=\"btn btn-info\">Upload</button>\
     </div>\
     </div>\
     \
@@ -64,34 +64,34 @@ function displayScr()
 <fieldset>\
 <!-- Text input-->\
 <div class=\"control-group\">\
-    <label class=\"control-label\" for=\"title\">Sender</label>\
+    <label class=\"control-label\" for=\"letterSender\">Sender</label>\
     <div class=\"controls\">\
-    <input id=\"letSnd\" name=\"title\" type=\"text\" placeholder=\"Name\" class=\"input-xlarge\">\
+    <input id=\"letterSender\" name=\"letterSender\" type=\"text\" placeholder=\"Name\" class=\"input-xlarge\">\
 \
     </div>\
     </div>\
     \
     <div class=\"control-group\">\
-    <label class=\"control-label\" for=\"title\">Receiver</label>\
+    <label class=\"control-label\" for=\"letterReceiver\">Receiver</label>\
     <div class=\"controls\">\
-    <input id=\"letRec\" name=\"title\" type=\"text\" placeholder=\"Name\" class=\"input-xlarge\">\
+    <input id=\"letterReceiver\" name=\"letterReceiver\" type=\"text\" placeholder=\"Name\" class=\"input-xlarge\">\
 \
     </div>\
     </div>\
 \
     <!-- Textarea -->\
     <div class=\"control-group\">\
-    <label class=\"control-label\" for=\"Description\">Content</label>\
+    <label class=\"control-label\" for=\"letterContent\">Letter</label>\
     <div class=\"controls\">\
-    <textarea id=\"Description\" name=\"Description\"></textarea>\
+    <textarea id=\"letterContent\" name=\"letterContent\"></textarea>\
     </div>\
     </div>\
     \
     <!-- Button -->\
     <div class=\"control-group\">\
-    <label class=\"control-label\" for=\"singlebutton\"></label>\
+    <label class=\"control-label\" for=\"letterUploadButton\"></label>\
     <div class=\"controls\">\
-    <button id=\"singlebutton\" name=\"singlebutton\" class=\"btn btn-info\">Upload</button>\
+    <button id=\"letterUploadButton\" name=\"letterUploadButton\" class=\"btn btn-info\">Upload</button>\
     </div>\
     </div>\
     \
@@ -109,26 +109,26 @@ function displayArt()
 <fieldset>\
 <!-- Text input-->\
 <div class=\"control-group\">\
-    <label class=\"control-label\" for=\"title\">Type</label>\
+    <label class=\"control-label\" for=\"artType\">Type</label>\
     <div class=\"controls\">\
-    <input id=\"title\" name=\"title\" type=\"text\" placeholder=\"Name\" class=\"input-xlarge\">\
+    <input id=\"artType\" name=\"artType\" type=\"text\" placeholder=\"Name\" class=\"input-xlarge\">\
 \
     </div>\
     </div>\
 \
     <!-- Textarea -->\
     <div class=\"control-group\">\
-    <label class=\"control-label\" for=\"Description\">Date</label>\
+    <label class=\"control-label\" for=\"artDescription\">Date</label>\
     <div class=\"controls\">\
-    <input type=\"text\" id=\"Description\" name=\"Description\">\
+    <input type=\"text\" id=\"artDescription\" name=\"artDescription\">\
     </div>\
     </div>\
     \
     <!-- Button -->\
     <div class=\"control-group\">\
-    <label class=\"control-label\" for=\"singlebutton\"></label>\
+    <label class=\"control-label\" for=\"artUploadButton\"></label>\
     <div class=\"controls\">\
-    <button id=\"singlebutton\" name=\"singlebutton\" class=\"btn btn-info\">Upload</button>\
+    <button id=\"artUploadButton\" name=\"artUploadButton\" class=\"btn btn-info\">Upload</button>\
     </div>\
     </div>\
     \
@@ -146,26 +146,26 @@ function displayAct()
 <fieldset>\
 <!-- Text input-->\
 <div class=\"control-group\">\
-    <label class=\"control-label\" for=\"title\">Title</label>\
+    <label class=\"control-label\" for=\"actTitle\">Title</label>\
     <div class=\"controls\">\
-    <input id=\"title\" name=\"title\" type=\"text\" placeholder=\"Name\" class=\"input-xlarge\">\
+    <input id=\"actTitle\" name=\"actTitle\" type=\"text\" placeholder=\"Name\" class=\"input-xlarge\">\
 \
     </div>\
     </div>\
 \
     <!-- Textarea -->\
     <div class=\"control-group\">\
-    <label class=\"control-label\" for=\"Description\">Location</label>\
+    <label class=\"control-label\" for=\"actLocation\">Location</label>\
     <div class=\"controls\">\
-    <input type=\"text\" id=\"Description\" name=\"Description\" class=\"input-xlarge\">\
+    <input type=\"text\" id=\"actLocation\" name=\"actLocation\" class=\"input-xlarge\">\
     </div>\
     </div>\
     \
     <!-- Button -->\
     <div class=\"control-group\">\
-    <label class=\"control-label\" for=\"singlebutton\"></label>\
+    <label class=\"control-label\" for=\"actUploadButton\"></label>\
     <div class=\"controls\">\
-    <button id=\"singlebutton\" name=\"singlebutton\" class=\"btn btn-info\">Upload</button>\
+    <button id=\"actUploadButton\" name=\"actUploadButton\" class=\"btn btn-info\">Upload</button>\
     </div>\
     </div>\
     \
