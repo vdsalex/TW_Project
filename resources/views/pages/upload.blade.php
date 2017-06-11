683d32ff375db02879a74dad2ae7937e6170bfb8
alinacoca <alinaacoca@yahoo.com>
2017-06-11 11:58:25 +0300

    html

--- resources/views/pages/upload.blade.php
+++ resources/views/pages/upload.blade.php
@@ -14,32 +14,121 @@
 
     <div class="container" id="mainContainer">
     @include ('includes.header')
-        <p>
-            Historically, stars have been important to civilizations throughout the world. They have been part of religious practices and used for celestial navigation and orientation. Many ancient astronomers believed that stars were permanently affixed to a heavenly sphere and that they were immutable. By convention, astronomers grouped stars into constellations and used them to track the motions of the planets and the inferred position of the Sun.[5] The motion of the Sun against the background stars (and the horizon) was used to create calendars, which could be used to regulate agricultural practices.[7] The Gregorian calendar, currently used nearly everywhere in the world, is a solar calendar based on the angle of the Earth's rotational axis relative to its local star, the Sun.
+        <p id="question">What do you want to upload?</p>
+        <div class="tab">
+            <button class="tablinks" onclick="openObject(event, 'Artefact')" id="defaultOpen">Artefact</button>
+            <button class="tablinks" onclick="openObject(event, 'Document')">Document</button>
+            <button class="tablinks" onclick="openObject(event, 'Letter')">Letter</button>
+            <button class="tablinks" onclick="openObject(event, 'Picture')">Picture</button>
+            <button class="tablinks" onclick="openObject(event, 'Video')">Video</button>
+        </div>
 
-            The oldest accurately dated star chart was the result of ancient Egyptian astronomy in 1534 BC.[8] The earliest known star catalogues were compiled by the ancient Babylonian astronomers of Mesopotamia in the late 2nd millennium BC, during the Kassite Period (ca. 1531–1155 BC).[9]
+        <div id="Artefact" class="tabcontent">
+            <h3>Artefact</h3>
+            <div class="contentObj">
+                <form >
+                    Name:<br>
+                    <input type="text" name="name">
+                    <br>
+                    Description:<br>
+                    <textarea rows="5" cols="50" >Please add a description column in the database</textarea>
+                    <br>
+                    Date of receiving:<br>
+                    <input type="text" name="receive_date">
+                    <br><br>
+                    <input type="file" name="image" class="form-control"  style="width:300px; background: rgba(75,195,230,0.45)" >
+                    <br>
+                    <input type="submit" value="Submit">
+                </form>
+            </div>
+        </div>
 
-            The first star catalogue in Greek astronomy was created by Aristillus in approximately 300 BC, with the help of Timocharis.[10] The star catalog of Hipparchus (2nd century BC) included 1020 stars, and was used to assemble Ptolemy's star catalogue.[11] Hipparchus is known for the discovery of the first recorded nova (new star).[12] Many of the constellations and star names in use today derive from Greek astronomy.
+        <div id="Document" class="tabcontent">
+            <h3>Document</h3>
+            <div class="contentObj">
+                <form >
+                    Name:<br>
+                    <input type="text" name="name">
+                    <br>
+                    Emission location:<br>
+                    <input type="text" name="location">
+                    <br>
+                    Emission date:<br>
+                    <input type="text" name="emission_date">
+                    <br><br>
+                    <input type="file" name="image" class="form-control"  style="width:300px; background: rgba(75,195,230,0.45)" >
+                    <br>
+                    <input type="submit" value="Submit">
+                </form>
 
-            In spite of the apparent immutability of the heavens, Chinese astronomers were aware that new stars could appear.[13] In 185 AD, they were the first to observe and write about a supernova, now known as the SN 185.[14] The brightest stellar event in recorded history was the SN 1006 supernova, which was observed in 1006 and written about by the Egyptian astronomer Ali ibn Ridwan and several Chinese astronomers.[15] The SN 1054 supernova, which gave birth to the Crab Nebula, was also observed by Chinese and Islamic astronomers.[16][17][18]
+            </div>
+        </div>
 
-            Medieval Islamic astronomers gave Arabic names to many stars that are still used today and they invented numerous astronomical instruments that could compute the positions of the stars. They built the first large observatory research institutes, mainly for the purpose of producing Zij star catalogues.[19] Among these, the Book of Fixed Stars (964) was written by the Persian astronomer Abd al-Rahman al-Sufi, who observed a number of stars, star clusters (including the Omicron Velorum and Brocchi's Clusters) and galaxies (including the Andromeda Galaxy).[20] According to A. Zahoor, in the 11th century, the Persian polymath scholar Abu Rayhan Biruni described the Milky Way galaxy as a multitude of fragments having the properties of nebulous stars, and also gave the latitudes of various stars during a lunar eclipse in 1019.[21]
+        <div id="Letter" class="tabcontent">
+            <h3>Letter</h3>
+            <div class="contentObj">
+                <form >
+                    Sender:<br>
+                    <input type="text" name="sender">
+                    <br>
+                    Receiver:<br>
+                    <input type="text" name="receiver">
+                    <br>
+                    Message:<br>
+                    <textarea rows="5" cols="50" ></textarea>
+                    <br>
+                    Date of writing<br>
+                    <input type="text" name="write_data">
+                    <br><br>
+                    <input type="file" name="image" class="form-control"  style="width:300px; background: rgba(75,195,230,0.45)" >
+                    <br>
+                    <input type="submit" value="Submit">
+                </form>
+            </div>
+        </div>
 
-            According to Josep Puig, the Andalusian astronomer Ibn Bajjah proposed that the Milky Way was made up of many stars that almost touched one another and appeared to be a continuous image due to the effect of refraction from sublunary material, citing his observation of the conjunction of Jupiter and Mars on 500 AH (1106/1107 AD) as evidence.[22] Early European astronomers such as Tycho Brahe identified new stars in the night sky (later termed novae), suggesting that the heavens were not immutable. In 1584 Giordano Bruno suggested that the stars were like the Sun, and may have other planets, possibly even Earth-like, in orbit around them,[23] an idea that had been suggested earlier by the ancient Greek philosophers, Democritus and Epicurus,[24] and by medieval Islamic cosmologists[25] such as Fakhr al-Din al-Razi.[26] By the following century, the idea of the stars being the same as the Sun was reaching a consensus among astronomers. To explain why these stars exerted no net gravitational pull on the Solar System, Isaac Newton suggested that the stars were equally distributed in every direction, an idea prompted by the theologian Richard Bentley.[27]
+        <div id="Picture" class="tabcontent">
+            <h3>Picture</h3>
+            <div class="contentObj">
+                <form >
+                    Description:<br>
+                    <textarea rows="4" cols="50" ></textarea>
+                    <br>
+                    Location:<br>
+                    <input type="text" name="title">
+                    <br>
+                    Creation date:<br>
+                    <input type="text" name="record_date">
+                    <br><br>
+                    <input type="file" name="image" class="form-control"  style="width:300px; background: rgba(75,195,230,0.45)" >
+                    <br>
+                    <input type="submit" value="Submit">
+                </form>
+            </div>
+        </div>
 
-            The Italian astronomer Geminiano Montanari recorded observing variations in luminosity of the star Algol in 1667. Edmond Halley published the first measurements of the proper motion of a pair of nearby "fixed" stars, demonstrating that they had changed positions since the time of the ancient Greek astronomers Ptolemy and Hipparchus.[23]
+        <div id="Video" class="tabcontent">
+            <h3>Video</h3>
+            <div class="contentObj">
+                <form >
+                    Title:<br>
+                    <input type="text" name="title">
+                    <br>
+                    Description:<br>
+                    <textarea rows="4" cols="50" ></textarea>
+                    <br>
+                    Record date:<br>
+                    <input type="text" name="record_date">
+                    <br><br>
+                    <input type="file" name="image" class="form-control"  style="width:300px; background: rgba(75,195,230,0.45)" >
+                    <br>
+                    <input type="submit" value="Submit">
+                </form>
+            </div>
+        </div>
 
-            William Herschel was the first astronomer to attempt to determine the distribution of stars in the sky. During the 1780s he established a series of gauges in 600 directions and counted the stars observed along each line of sight. From this he deduced that the number of stars steadily increased toward one side of the sky, in the direction of the Milky Way core. His son John Herschel repeated this study in the southern hemisphere and found a corresponding increase in the same direction.[28] In addition to his other accomplishments, William Herschel is also noted for his discovery that some stars do not merely lie along the same line of sight, but are also physical companions that form binary star systems.
-
-            The science of stellar spectroscopy was pioneered by Joseph von Fraunhofer and Angelo Secchi. By comparing the spectra of stars such as Sirius to the Sun, they found differences in the strength and number of their absorption lines—the dark lines in a stellar spectra caused by the atmosphere's absorption of specific frequencies. In 1865 Secchi began classifying stars into spectral types.[29] However, the modern version of the stellar classification scheme was developed by Annie J. Cannon during the 1900s.
-
-            The first direct measurement of the distance to a star (61 Cygni at 11.4 light-years) was made in 1838 by Friedrich Bessel using the parallax technique. Parallax measurements demonstrated the vast separation of the stars in the heavens.[23] Observation of double stars gained increasing importance during the 19th century. In 1834, Friedrich Bessel observed changes in the proper motion of the star Sirius and inferred a hidden companion. Edward Pickering discovered the first spectroscopic binary in 1899 when he observed the periodic splitting of the spectral lines of the star Mizar in a 104-day period. Detailed observations of many binary star systems were collected by astronomers such as Friedrich Georg Wilhelm von Struve and S. W. Burnham, allowing the masses of stars to be determined from computation of orbital elements. The first solution to the problem of deriving an orbit of binary stars from telescope observations was made by Felix Savary in 1827.[30] The twentieth century saw increasingly rapid advances in the scientific study of stars. The photograph became a valuable astronomical tool. Karl Schwarzschild discovered that the color of a star and, hence, its temperature, could be determined by comparing the visual magnitude against the photographic magnitude. The development of the photoelectric photometer allowed precise measurements of magnitude at multiple wavelength intervals. In 1921 Albert A. Michelson made the first measurements of a stellar diameter using an interferometer on the Hooker telescope at Mount Wilson Observatory.[31]
-
-            Important theoretical work on the physical structure of stars occurred during the first decades of the twentieth century. In 1913, the Hertzsprung-Russell diagram was developed, propelling the astrophysical study of stars. Successful models were developed to explain the interiors of stars and stellar evolution. Cecilia Payne-Gaposchkin first proposed that stars were made primarily of hydrogen and helium in her 1925 PhD thesis.[32] The spectra of stars were further understood through advances in quantum physics. This allowed the chemical composition of the stellar atmosphere to be determined.[33]
-
-            With the exception of supernovae, individual stars have primarily been observed in the Local Group,[34] and especially in the visible part of the Milky Way (as demonstrated by the detailed star catalogues available for our galaxy).[35] But some stars have been observed in the M100 galaxy of the Virgo Cluster, about 100 million light years from the Earth.[36] In the Local Supercluster it is possible to see star clusters, and current telescopes could in principle observe faint individual stars in the Local Group[37] (see Cepheids). However, outside the Local Supercluster of galaxies, neither individual stars nor clusters of stars have been observed. The only exception is a faint image of a large star cluster containing hundreds of thousands of stars located at a distance of one billion light years[38]—ten times further than the most distant star cluster previously observed.
-        </p>
     </div>
 
 </body>
 </html>
+{!! Html::script('js/upload.js') !!}
