f7b33e46b0a5e04e18b200ca4f2d1e2c404dfdc8
Leustean Andrei <leuandrei01@gmail.com>
2017-06-13 11:26:50 +0300

    Fixed radio buttons in advanced search

--- resources/views/pages/advanced_search.blade.php
+++ resources/views/pages/advanced_search.blade.php
@@ -18,11 +18,11 @@
         <div id="mySidenav1" class="sidenav">
             <br>
             <form>
-                <a ><input type="radio">Artefact</a>
-                <a ><input type="radio">Document</a>
-                <a ><input type="radio">Letter</a>
-                <a ><input type="radio">Picture</a>
-                <a ><input type="radio">Video</a>
+                <a ><input type="radio" name="memory">Artefact</a>
+                <a ><input type="radio" name="memory">Document</a>
+                <a ><input type="radio" name="memory">Letter</a>
+                <a ><input type="radio" name="memory">Picture</a>
+                <a ><input type="radio" name="memory">Video</a>
             </form>
             <a class="closebtn" onclick="closeNav1()">
                 <input type="submit" value="Submit">
@@ -31,9 +31,9 @@
         <div id="mySidenav2" class="sidenav">
             <br>
             <form>
-                <a ><input type="radio">1</a>
-                <a ><input type="radio">2</a>
-                <a ><input type="radio">3</a>
+                <a ><input type="radio" name="grad">1</a>
+                <a ><input type="radio" name="grad">2</a>
+                <a ><input type="radio" name="grad">3</a>
             </form>
             <a class="closebtn" onclick="closeNav2()">
                 <input type="submit" value="Submit">
