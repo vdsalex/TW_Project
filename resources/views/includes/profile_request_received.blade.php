e3a795768cbc9f1f57893d769c02197bd602e87c
vdsalex <voaides.alex@gmail.com>
2017-06-19 01:47:47 +0300

    PHP Laravel code for sent requests, received requests.

---
+++ resources/views/includes/profile_request_received.blade.php
@@ -0,0 +1,11 @@
+<div class="request">
+    <img src="" alt="Requester's Photo" class="requesterPhoto">
+    <div class="requestDiv">
+        <p class="requesterName"></p>
+        <p class="requestMessage"> sent you a request.</p>
+    </div>
+    <div class="responseBtns">
+        <button class="btn btn-default" onclick="moveToFriends(this.parentElement.parentElement)">Accept</button>
+        <button class="btn btn-default" onclick="deleteRequest(this.parentElement.parentElement)">Reject</button>
+    </div>
+</div>
