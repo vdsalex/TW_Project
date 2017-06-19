ac054c7b708486b8c3ab1a0b3360f591ff8b8110
vdsalex <voaides.alex@gmail.com>
2017-06-20 01:47:59 +0300

    Added some buttons to memory jumbotrons.

--- resources/views/includes/import_into_account.blade.php
+++ resources/views/includes/import_into_account.blade.php
@@ -12,7 +12,7 @@
     <div class="jumbotron" style="margin-top: 80px !important;">
         <div class="poster">
             <div class="profile-photo">
-                <a href=http://localhost:8000/profile><img src="content/fat-frumos.jpg" alt="Profile Photo" width=50" height="46" ></a>
+                <a href=http://localhost:8000/profile><img src="content/fat-frumos.jpg" alt="Profile Photo" width=70" height="66" ></a>
             </div>
             <p  align="left"><a href=http://localhost:8000/profile> &nbsp; FirstName LastName </a> added a photo.</p>
             <button class="btn btn-default deleteBtn">Delete This Memory</button>
@@ -20,14 +20,10 @@
         <input type="button" class="button" value="Import into account" style="background-color: #4CAF52;
                 color: white; padding: 15px 32px; text-align: center;font-size: 16px;margin: 4px 2px;cursor: pointer; float: right;padding-top: 5px !important;">
         <img  src="content/fat-frumos.jpg" alt="Photo" class="img-rounded"><br><br>
-
-        <div class="rightContainer">
-            <button class="btn info" onclick="hideF(this)">Information</button>
-            <div class="hideBut">
-                <p>Description: {{$entry['description']}}</p>
-                <p>Location: {{$entry['location']}}</p>
-                <p>Creation date: {{$entry['snap_date']}}</p>
-            </div>
+        <div class="btnsContainerz">
+            <button class="btn info" onclick="hideF(this)">Description</button>
+            <button class="btn info" onclick="hideF(this)">Location</button>
+            <button class="btn info" onclick="hideF(this)">Receiving Date</button>
         </div>
     </div>
 @endforeach
--- resources/views/includes/memories_facebook.blade.php
+++ resources/views/includes/memories_facebook.blade.php
@@ -8,7 +8,7 @@
     <div class="jumbotron" style="margin-top: 80px !important;">
         <div class="poster">
             <div class="profile-photo">
-                <a href="{{route('profile')}}"><img src="{{route('account.image',['userId'=>Auth::user()->id,'username'=>Auth::user()->username])}}" alt="Profile Photo" width=50" height="46" ></a>
+                <a href="{{route('profile')}}"><img src="{{route('account.image',['userId'=>Auth::user()->id,'username'=>Auth::user()->username])}}" alt="Profile Photo" width=70" height="66" ></a>
             </div>
             <p  align="left"><a href={{route('profile')}}> &nbsp; You </a> added a photo.</p>
         </div>
@@ -19,12 +19,10 @@
         </form>
         <img  src="{{$entry['URL']}}" alt="Photo" class="img-rounded"><br><br>
 
-        <div class="rightContainer">
-            <button class="btn info" onclick="hideF(this)">Information</button>
-            <div class="hideBut">
-                <p>Description: {{$entry['name']}}</p>
-                <p>Location: {{$entry['location']}}</p>
-            </div>
+        <div class="btnsContainerz">
+            <button class="btn info" onclick="hideF(this)">Description</button>
+            <button class="btn info" onclick="hideF(this)">Location</button>
+            <button class="btn info" onclick="hideF(this)">Receiving Date</button>
         </div>
     </div>
 @endforeach
